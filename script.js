c4392c45ee1325a85a6694925dfdcaeda919b888
djiankf <fbdjian@gmail.com>
2025-01-24 13:34:33 -0500

    update

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function toggleDescription(element) {
     const description = element.nextElementSibling;
     if (description.style.display === "none" || description.style.display === "") {
-        description.style.display = "block";
+        description.style.display = "block"; // Show the description
     } else {
-        description.style.display = "none";
+        description.style.display = "none"; // Hide the description
     }
 }
